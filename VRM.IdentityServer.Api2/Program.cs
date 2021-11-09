35e9dd8bfc40ef2bee57b752b10094ea45ccb9c9
Rustam Valiev <electroman232@gmail.com>
2021-11-09 21:14:47 +0300

    Updated packages. Initial configuration for APIs.

--- VRM.IdentityServer.Api2/Program.cs
+++ VRM.IdentityServer.Api2/Program.cs
@@ -4,6 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Здесь добавляем аутентификацию с помощью JWT токена
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", config =>
+    {
+        // Кто мы есть
+        config.Audience = "ApiTwo";
+        // Адрес сервера аутентификации
+        config.Authority = "https://localhost:5001";
+    });
+
+// Добавляем возможность использовать HttpClient
+builder.Services.AddHttpClient();
+
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
 {
